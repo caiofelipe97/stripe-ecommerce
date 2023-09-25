cfad4590349f005b97ddc95bf95e1723c4ebf79e
caiofelipe97 <caiofelipee17@gmail.com>
2023-09-25 17:11:46 -0300

    Home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,9 @@ import { globalStyles } from "@/styles/global";
 import type { AppProps } from "next/app";
 
 import logoImg from "../assets/logo.svg";
+
 import { Container, Header } from "@/styles/pages/app";
+import Image from "next/image";
 
 globalStyles();
 
@@ -10,7 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg} alt="" />
       </Header>
       <Component {...pageProps} />
     </Container>
