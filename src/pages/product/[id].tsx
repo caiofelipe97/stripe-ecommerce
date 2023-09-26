fbca043d83ee211bec765ea65325fb04aec42a76
caiofelipe97 <caiofelipee17@gmail.com>
2023-09-26 19:21:15 -0300

    Create product's page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,26 @@
-import { useRouter } from "next/router"
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "@/styles/pages/product";
+import { useRouter } from "next/router";
 
 export default function Product() {
-    const { query } = useRouter()
-    return (
-      <h1>Product: {JSON.stringify(query)}</h1>
-    )
-  }
-  
+  const { query } = useRouter();
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+          suscipit suscipit risus, a interdum quam malesuada nec. Aliquam quis
+          convallis arcu. Praesent vel hendrerit turpis. Nulla facilisi. Morbi
+          vel dui a libero volutpat accumsan ut id quam
+        </p>
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
+}
