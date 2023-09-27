3396228c9ab9ced63ec6a00f184201b2215ed8a3
caiofelipe97 <caiofelipee17@gmail.com>
2023-09-27 18:16:53 -0300

    Create Success page structure

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,6 +1,17 @@
+import { ImageContainer, SuccessContainer } from "@/styles/pages/success";
+import Link from "next/link";
+
 export default function Success() {
-    return (
-      <h1>Success</h1>
-    )
-  }
-  
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+      <ImageContainer></ImageContainer>
+      <p>
+        Uhuul <strong>Caio Felipe</strong>, sua{" "}
+        <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua
+        casa.
+      </p>
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
+  );
+}
